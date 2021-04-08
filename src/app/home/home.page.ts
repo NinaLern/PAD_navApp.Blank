ed11bb4f0e437d82f6e5b86c73100dab217d7f7a
Nina Chuang <aa070312@au.edu.tw>
2021-04-08 14:54:29 +0800

    0408-classwork

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,32 +7,22 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  lists = [
-    {id: 2330, name: '台積電', price: 147.0, PER: 14.44, PBR: 3.65,
-        yield: 3.06, Desc: '晶圓製造'},
-    {id: 1301, name: '台塑', price: 76.5, PER: 27.03, PBR: 1.7,
-        yield: 2.22, Desc: '聚氯乙烯(PVC)、氯乙烯等塑膠製品'},
-    {id: 2002, name: '中鋼', price: 25.8, PER: 18.04, PBR: 1.33,
-        yield: 3.88, Desc: '鋼品設計製造買賣儲運及其他相關業務'},
-    {id: 3045, name: '台灣大', price: 108.0, PER: 19.42, PBR: 6.13,
-        yield: 5.19, Desc: '通訊業'},
-    {id: 2454, name: '聯發科', price: 421.5, PER: 14.03, PBR: 2.68,
-        yield: 3.56, Desc: '多媒體IC 、電腦週邊IC、其他IC'},
-    {id: 2317, name: '鴻海', price: 93.1, PER: 10.52, PBR: 1.48,
-        yield: 1.93, Desc: '電腦系統設備連接器等之開發設計製造'},
-    {id: 3008, name: '大立光', price: 2695.0, PER: 18.6, PBR: 7.83,
-        yield: 1.06, Desc: '各式光學鏡頭模組研發設計生產銷售'}
-  ];
 
+ places = [
+  {id: '1', title: '幾米主題南港站', photoURL: 'g_me.jpg', intro: '受到各方廣大喜愛的幾米主題裝置藝術，就藏身在捷運板南線的南港站之中。南港捷運站大膽引進享譽國際的幾米繪畫，完整表現出捷運站的現代性'},
+  {id: '2', title: '140高地公園', photoURL: 'high_land.jpg', intro: '140高地公園位於台北市文山區萬美里境內，萬寧街北側，介於萬寧街與萬美街2段間的丘陵地，最高海拔為138公尺的枹子腳山（枹仔坑山），山頂視野遼闊'},
+  {id: '3', title: '通化公園', photoURL: 'th_park.jpg', intro: '本公園位於文昌街與通化街口，於民國76年建立，原命名為「文通公園」，面積1180平方公尺，後改「文通公園」為「通化公園」'},
+  {id: '4', title: '世貿公園', photoURL: 'wtc_park.jpg', intro: '為提供市民更多的停車空間，停管處新建南港區世貿公園地下停車場，並於民國100年簡易綠美化，公園面積約1.2公頃，位於南港區經貿二路106巷'},
+];
   // 建構子宣告Router服務引數
   // 參數router:類型Router(外部功能)
   constructor(private router: Router) {}
 
   // 切換到detail頁面的時候，將代號傳過去。
   // 參數stockid（股票代號）：類型（數字）
-  detail(stockid:number) {
+  detail(placesid: number) {
     // ['網址',{要傳送的代號跟名稱}]
-    this.router.navigate(['detail', {id: stockid}]);
+    this.router.navigate(['detail', {id: placesid}]);
   }
 
 }
